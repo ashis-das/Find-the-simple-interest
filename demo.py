51f1561d52afdb0e66f842953b3ed8e825723b0d
Ashis Das <105511844+ashis-das@users.noreply.github.com>
2022-11-20 15:06:56 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,7 @@
+#  Find the simple interest
+#take three user input number
+principal=float(input("enter the principal amount"))
+time=float(input("enter the time period"))
+rate=float(input("enter the rate of interest"))
+si = (principal * time * rate)/100 
+print('The Simple Interest is', si)
